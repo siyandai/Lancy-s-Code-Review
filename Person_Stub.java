ba89d8cdc43c78df41593677304d932dd375d405
Xiaodanlan <lanxiaodan@gmail.com>
2012-10-20 22:23:26 -0400

    new Person_Stub

--- Person_Stub.java
+++ Person_Stub.java
@@ -4,10 +4,52 @@
  */
 package project3task5client;
 
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
 /**
  *
  * @author Lancy
  */
-public class Person_Stub {
-    
+public class Person_Stub implements Person {
+    Socket socket;
+    ObjectOutputStream o;
+    ObjectInputStream i;
+    RequestReplyMessage message;
+
+    public Person_Stub() throws Exception {
+    }
+
+    Person_Stub(RemoteObjectReference remoteObject) {
+        message.setRemoteObjectReference(remoteObject);
+    }
+
+    @Override
+    public int getID() throws Exception {
+        socket = new Socket();
+        o = new ObjectOutputStream(socket.getOutputStream());
+        //Getting person ID
+        o.writeObject("ID");
+        o.flush();
+        i = new ObjectInputStream(socket.getInputStream());
+        //Read the result of person ID
+        int ret = i.readInt();
+        socket.close();
+        return ret;
+    }
+
+    @Override
+    public String getName() throws Exception {
+        socket = new Socket("localhost", 9000);
+        o = new ObjectOutputStream(socket.getOutputStream());
+        //Getting person name
+        o.writeObject("name");
+        o.flush();
+        i = new ObjectInputStream(socket.getInputStream());
+        //Read the result of person name
+        String ret = (String) (i.readObject());
+        socket.close();
+        return (String) ret;
+    }
 }
